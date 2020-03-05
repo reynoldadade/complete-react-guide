c06dadab6866fdb50a2e1c7f5c9668695002e1d9
Reyold Osei Adade <reynoldadade@gmail.com>
2020-03-05 15:03:46 +0000

    change handlers

--- src/App.js
+++ src/App.js
@@ -5,28 +5,48 @@ import Person from './Person/Person';
 class App extends Component {
 	state = {
 		persons: [
-			{ name: 'Rey', age: 27 },
-			{ name: 'Bansah', age: 29 },
+			{ id: 1, name: 'Rey', age: 27 },
+			{ id: 2, name: 'Bansah', age: 29 },
 		],
+		showPersons: false,
 	};
 
 	switchNameHandler = () => {
 		//	console.log('Was clicked');
 		this.setState({
 			persons: [
-				{ name: 'Reynold', age: 26 },
-				{ name: 'Selassie', age: 29 },
+				{ id: 1, name: 'Reynold', age: 26 },
+				{ id: 2, name: 'Selassie', age: 29 },
 			],
 		});
 	};
 
-	nameChangedHandler = event => {
-		this.setState({
-			persons: [
-				{ name: event.target.value, age: 26 },
-				{ name: 'Selassie', age: 29 },
-			],
+	nameChangedHandler = (id, event) => {
+		const personIndex = this.state.persons.findIndex(p => {
+			return p.id === id;
 		});
+
+		console.log('personid', id);
+
+		const person = { ...this.state.persons[personIndex] };
+		person.name = event.target.value;
+		const persons = [...this.state.persons];
+		persons[personIndex] = person;
+		this.setState({
+			persons: persons,
+		});
+	};
+
+	togglePersonsHandler = () => {
+		this.setState({
+			showPersons: !this.state.showPersons,
+		});
+	};
+
+	deletePersonHandler = personIndex => {
+		const persons = [...this.state.persons];
+		persons.splice(personIndex, 1);
+		this.setState({ persons: persons });
 	};
 	render() {
 		const style = {
@@ -36,23 +56,38 @@ class App extends Component {
 			padding: '8px',
 			cursor: 'pointer',
 		};
+
+		let persons = null;
+		if (this.state.showPersons) {
+			persons = (
+				<div>
+					{this.state.persons.map((person, index) => {
+						return (
+							<Person
+								click={this.deletePersonHandler.bind(
+									this,
+									index
+								)}
+								name={person.name}
+								age={person.age}
+								key={person.id}
+								changed={this.nameChangedHandler.bind(
+									this,
+									person.id
+								)}
+							/>
+						);
+					})}
+				</div>
+			);
+		}
 		return (
 			<div className="App">
 				<h1>Hi I am react app</h1>
-				<Person
-					name={this.state.persons[0].name}
-					age={this.state.persons[0].age}
-					changed={this.nameChangedHandler}
-				>
-					This is to check for children
-				</Person>
-				<Person
-					name={this.state.persons[1].name}
-					age={this.state.persons[1].age}
-				/>
-				<button style={style} onClick={this.switchNameHandler}>
+				<button style={style} onClick={this.togglePersonsHandler}>
 					Switch Name
 				</button>
+				{persons}
 			</div>
 		);
 	}
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,10 +4,9 @@ import './Person.css';
 const Person = props => {
 	return (
 		<div className="Person">
-			<p>
+			<p onClick={props.click}>
 				I am a {props.name} and I am {props.age} years old!
 			</p>
-			<p>{props.children}</p>
 			<input type="text" onChange={props.changed} />
 		</div>
 	);
