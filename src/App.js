049ce563da340b51c9445b7067ef02fc772b0dbd
Reyold Osei Adade <reynoldadade@gmail.com>
2020-03-05 11:35:54 +0000

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi I am react app</h1>
-      </div>
-    );
-  }
+	state = {
+		persons: [
+			{ name: 'Rey', age: 27 },
+			{ name: 'Bansah', age: 29 },
+		],
+	};
+
+	switchNameHandler = () => {
+		//	console.log('Was clicked');
+		this.setState({
+			persons: [
+				{ name: 'Reynold', age: 26 },
+				{ name: 'Selassie', age: 29 },
+			],
+		});
+	};
+	render() {
+		return (
+			<div className="App">
+				<h1>Hi I am react app</h1>
+				<Person
+					name={this.state.persons[0].name}
+					age={this.state.persons[0].age}
+				>
+					This is to check for children
+				</Person>
+				<Person
+					name={this.state.persons[1].name}
+					age={this.state.persons[1].age}
+				/>
+				<button onClick={this.switchNameHandler}>Switch Name</button>
+			</div>
+		);
+	}
 }
 
 export default App;
