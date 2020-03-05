f5c417a0dc770af2e7e1af79dcad143acac7e2c9
Reyold Osei Adade <reynoldadade@gmail.com>
2020-03-05 12:55:29 +0000

    styling

--- src/App.js
+++ src/App.js
@@ -19,13 +19,30 @@ class App extends Component {
 			],
 		});
 	};
+
+	nameChangedHandler = event => {
+		this.setState({
+			persons: [
+				{ name: event.target.value, age: 26 },
+				{ name: 'Selassie', age: 29 },
+			],
+		});
+	};
 	render() {
+		const style = {
+			backgroudColor: 'white',
+			font: 'inherit',
+			border: '1px solid blue',
+			padding: '8px',
+			cursor: 'pointer',
+		};
 		return (
 			<div className="App">
 				<h1>Hi I am react app</h1>
 				<Person
 					name={this.state.persons[0].name}
 					age={this.state.persons[0].age}
+					changed={this.nameChangedHandler}
 				>
 					This is to check for children
 				</Person>
@@ -33,7 +50,9 @@ class App extends Component {
 					name={this.state.persons[1].name}
 					age={this.state.persons[1].age}
 				/>
-				<button onClick={this.switchNameHandler}>Switch Name</button>
+				<button style={style} onClick={this.switchNameHandler}>
+					Switch Name
+				</button>
 			</div>
 		);
 	}
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import './Person.css';
 
 const Person = props => {
 	return (
-		<div>
+		<div className="Person">
 			<p>
 				I am a {props.name} and I am {props.age} years old!
 			</p>
 			<p>{props.children}</p>
+			<input type="text" onChange={props.changed} />
 		</div>
 	);
 };
